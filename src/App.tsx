d3a13c50a35e7f37fe4b66bd8d1f1b268359550a
Devashish Kaluvakolanu <kaldevas@udel.edu>
2024-09-09 23:51:18 -0400

    Final Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,6 +8,50 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript - Devashish
             </header>
+            <h1>This is Task 3</h1>
+            <img
+                src="C:\Users\devas\Downloads\Task3-CISC275.jpeg"
+                alt="This is a picture for Task 3."
+            />
+            <ol>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            ;
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            className="rectangle"
+                            style={{
+                                width: "100%",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                        First column.
+                    </Col>
+                    <Col>
+                        <div
+                            className="rectangle"
+                            style={{
+                                width: "100%",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                        Second column.
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
